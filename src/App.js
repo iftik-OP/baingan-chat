18887d80646432497b628459bb342d4f44866c6b
Iftikhar Ahmad <iftikharahmad786.mgs@gmail.com>
2023-12-16 22:35:11 +0530

    Add @vercel/speed-insights package

--- src/App.js
+++ src/App.js
@@ -33,6 +33,7 @@ function App() {
           <Route path="register" element={<Register />} />
         </Route>
       </Routes>
+      <SpeedInsights />
     </BrowserRouter>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { AuthContextProvider } from "./context/AuthContext";
 import { ChatContextProvider } from "./context/ChatContext";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
